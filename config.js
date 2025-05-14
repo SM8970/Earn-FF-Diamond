081c522106021ba8c3865ac7699007b91a3dbc77
SM8970 <smgaming8970@gmail.com>
2025-05-14 12:57:53 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,23 @@
-// Replace with your actual Firebase project configuration
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCR96FXvw6tpEqx8uOWV90xC02Ln-8WJEw",
+    authDomain: "earn-ff-diamond.firebaseapp.com",
+    projectId: "earn-ff-diamond",
+    storageBucket: "earn-ff-diamond.firebasestorage.app",
+    messagingSenderId: "864313846671",
+    appId: "1:864313846671:web:3604104e56a587b82db9fa",
+    measurementId: "G-1MRSZHDJB0"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
